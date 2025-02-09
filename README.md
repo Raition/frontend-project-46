c5d657e561e2953128ba0a8638022400446c1509
Rodion <marpleway@Noutbuk-Rodion.local>
2025-02-09 16:50:33 +0300

    add badge test-coverage

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Raition/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Raition/frontend-project-46/actions)
+
+[![Node CI](https://github.com/Raition/frontend-project-46/actions/workflows/test-check.yml/badge.svg)](https://github.com/Raition/frontend-project-46/actions/workflows/test-check.yml)
+
 [![Maintainability](https://api.codeclimate.com/v1/badges/66bb22d9b46099f6ce93/maintainability)](https://codeclimate.com/github/Raition/frontend-project-46/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/66bb22d9b46099f6ce93/test_coverage)](https://codeclimate.com/github/Raition/frontend-project-46/test_coverage)
 
 [![asciicast](https://asciinema.org/a/0we9L2xOboXJSwkW5bFrNO2JF.svg)](https://asciinema.org/a/0we9L2xOboXJSwkW5bFrNO2JF)
--- src/diff.js
+++ src/diff.js
@@ -1,30 +1,30 @@
 const diff = (obj1, obj2) => {
-  const cash = [];
-  const res = [];
+  const processedKeys = [];
+  const result = [];
   // eslint-disable-next-line no-restricted-syntax
   for (const [key, value] of Object.entries(obj1)) {
     if (Object.hasOwn(obj2, key) && obj2[key] === value) {
       console.log(`     ${key}: ${value}`);
-      res.push('=');
-      cash.push(key);
+      result.push('=');
+      processedKeys.push(key);
     } else if (Object.hasOwn(obj2, key) && obj2[key] !== value) {
       console.log(`  -  ${key}: ${value}`);
       console.log(`  +  ${key}: ${obj2[key]}`);
-      res.push('-', '+');
-      cash.push(key);
+      result.push('-', '+');
+      processedKeys.push(key);
     } else {
       console.log(`  -  ${key}: ${value}`);
-      res.push('-');
+      result.push('-');
     }
   }
   // eslint-disable-next-line no-restricted-syntax
   for (const [key, value] of Object.entries(obj2)) {
-    if (!cash.includes(key)) {
+    if (!processedKeys.includes(key)) {
       console.log(`  +  ${key}: ${value}`);
-      res.push('+');
+      result.push('+');
     }
   }
-  return res.join('');
+  return result.join('');
 };
 
 export default diff;
